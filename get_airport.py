b6df04da87596d9a6e4df3e1affbf39d6fbe24c7
Kirill Popov <kpopov@griddynamics.com>
2015-03-20 18:51:14 +0300

    Got rid of temp zip file on disk

--- get_airport.py
+++ get_airport.py
@@ -4,10 +4,10 @@ import sys
 import requests
 import base64
 import zipfile
-import os
+import StringIO
 
 if len(sys.argv) < 2:
-    print("Usage: {0} <airport_ICAO_code>".format(sys.argv[0]))
+    sys.stderr.write("Usage: {0} <airport_ICAO_code>\n".format(sys.argv[0]))
     sys.exit(1)
 
 ap_icao = str(sys.argv[1])
@@ -17,18 +17,16 @@ ap_json = requests.get(ap_request).json()
 
 sc_id = ap_json['airport'].get('recommendedSceneryId', None)
 if not sc_id:
-    print("Airport {0} has no recommendedSceneryId".format(ap_icao))
+    sys.stderr.write(
+        "Airport {0} has no recommendedSceneryId\n".format(ap_icao))
     sys.exit(1)
 
 sc_request = "http://gateway.x-plane.com/apiv1/scenery/{0}".format(sc_id)
 sc_json = requests.get(sc_request).json()
-ap_zip_file_name = "{0}.zip".format(ap_icao)
-with open(ap_zip_file_name, 'w') as ap_zip_file:
-    ap_zip_file.write(base64.b64decode(sc_json['scenery']['masterZipBlob']))
+ap_zip_stringio = StringIO.StringIO(
+    base64.b64decode(sc_json['scenery']['masterZipBlob']))
 
-with zipfile.ZipFile(ap_zip_file_name, 'r') as ap_zip:
+with zipfile.ZipFile(ap_zip_stringio, 'r') as ap_zip:
     ap_zip.extract("{0}.dat".format(ap_icao))
-
-os.remove(ap_zip_file_name)
 
 sys.exit(0)
